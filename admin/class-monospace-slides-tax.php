cef51ea2f49439ebab12139bf1014c8056af6ede
Mike <mikeinmonospace@gmail.com>
2019-08-09 12:21:53 +0300

    Create the class file for the custom taxonomy functionality.

---
+++ admin/class-monospace-slides-tax.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The admin-specific functionality for creating a custom taxonomy.
+ *
+ * @link       https://www.mikeinmonospace.com/
+ * @since      1.0.0
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ */
+
+/**
+ * The admin-specific functionality for creating a custom taxonomy.
+ *
+ * Defines all the functions needed to register and manage a custom taxonomy.
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ * @author     Mike In Monospace <mikeinmonospace@gmail.com>
+ */
+class Monospace_Slides_TAX {
+
+
+
+}
