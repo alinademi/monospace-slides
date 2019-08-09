374c39809c78275ed3c37de18ebcdda743e062c5
Mike <mikeinmonospace@gmail.com>
2019-08-09 12:12:52 +0300

    Create the class file for the custom post type functionality.

---
+++ admin/class-monospace-slides-cpt.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The admin-specific functionality for creating a custom post type.
+ *
+ * @link       https://www.mikeinmonospace.com/
+ * @since      1.0.0
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ */
+
+/**
+ * The admin-specific functionality for creating a custom post type.
+ *
+ * Defines all the functionality related to creating and managing a custom post type.
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ * @author     Mike In Monospace <mikeinmonospace@gmail.com>
+ */
+class Monospace_Slides_CPT {
+
+
+
+}
