3736691e095355889612cab8767f5c1a6b94d163
Mike <mikeinmonospace@gmail.com>
2019-08-11 17:03:41 +0300

    Update the custom post type meta class file with initial code.

--- admin/class-monospace-slides-cpt-meta.php
+++ admin/class-monospace-slides-cpt-meta.php
@@ -20,6 +20,55 @@
  */
 class Monospace_Slides_CPT_Meta {
 
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
 
+	/**
+	 * The post type screen on which the metabox will be visible.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @var    array   $version    The post type screen on which the metabox will be visible.
+	 */
+	private $screen = array( 'mnsp_slide' );
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since 1.0.0
+	 * @param string $version    The version of this plugin.
+	 */
+	public function __construct( $version ) {
+
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin-facing side of the custom post type meta box.
+	 *
+	 * @since 1.0.0
+	 */
+	public function enqueue_styles() {
+
+		// CSS rules for Color Picker.
+		wp_enqueue_style( 'wp-color-picker' );
+
+	}
+
+	/**
+	 * Register the Javascript for the admin-facing side of the custom post type meta box.
+	 *
+	 * @since 1.0.0
+	 */
+	public function enqueue_scripts() {
+
+	}
 
 }
