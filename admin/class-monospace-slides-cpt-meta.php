5e3be007baf4aed71c73335e083ea88905e7b012
Mike <mikeinmonospace@gmail.com>
2019-08-11 17:01:56 +0300

    Create the class file for the custom meta box functionality.

---
+++ admin/class-monospace-slides-cpt-meta.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The admin-specific functionality for the slide options metabox.
+ *
+ * @link       https://www.mikeinmonospace.com/
+ * @since      1.0.0
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ */
+
+/**
+ * The admin-specific functionality for the slide options metabox.
+ *
+ * Defines all the functionality related to the slide options metabox.
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ * @author     Mike In Monospace <mikeinmonospace@gmail.com>
+ */
+class Monospace_Slides_CPT_Meta {
+
+
+
+}
