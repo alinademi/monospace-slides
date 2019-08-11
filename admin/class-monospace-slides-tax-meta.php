cb7cb9b1c8d03b3fcbcf0f3ffa0fd1e707bdc1cf
Mike <mikeinmonospace@gmail.com>
2019-08-11 17:17:40 +0300

    Create the class file for the custom taxonomy term metadata.

---
+++ admin/class-monospace-slides-tax-meta.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The admin-specific functionality for the slider taxonomy term metadata.
+ *
+ * @link       https://www.mikeinmonospace.com/
+ * @since      1.0.0
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ */
+
+/**
+ * The admin-specific functionality for the slider taxonomy term metadata.
+ *
+ * Defines all the functionality related to the slider taxonomy term metadata options.
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/admin
+ * @author     Mike In Monospace <mikeinmonospace@gmail.com>
+ */
+class Monospace_Slides_TAX_Meta {
+
+
+
+}
