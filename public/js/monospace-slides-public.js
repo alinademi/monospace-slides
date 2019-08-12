28c718babfa3f6aef2745cccae6560e9b14c1d41
Mike <mikeinmonospace@gmail.com>
2019-08-12 13:04:03 +0300

    Update the public facing JS with code for the functionality of a single slide.

--- public/js/monospace-slides-public.js
+++ public/js/monospace-slides-public.js
@@ -6,4 +6,48 @@
 	 * should reside in this file.
 	 */
 
+	var slides  = document.querySelectorAll( '.mnsp-slide' );
+
+	slides.forEach( function( slide ) {
+
+		var $this 		= slide,
+			title 		= $this.querySelector( '.mnsp-slide-title' ),
+			subTitle 	= $this.querySelector( '.mnsp-slide-subtitle' ),
+			desc 		= $this.querySelector( '.mnsp-slide-desc' ),
+			cta 		= $this.querySelector( '.mnsp-slide-cta' ),
+			featured 	= $this.querySelector( '.mnsp-slide-featured' );
+
+		if ( title ) {
+			title.style.color = $this.dataset.mnspSlideTitleColor;
+		}
+
+		if ( subTitle ) {
+			subTitle.style.color = $this.dataset.mnspSlideSubtitleColor;
+		}
+
+		if ( desc ) {
+			desc.style.color = $this.dataset.mnspSlideDescColor;
+		}
+
+		if ( cta ) {
+			cta.style.color = $this.dataset.mnspSlideCtaTextColor;
+			cta.style.backgroundColor = $this.dataset.mnspSlideCtaBgColor;
+
+			cta.addEventListener( 'mouseover', function() {
+				$this.querySelector( '.mnsp-slide-cta' ).style.backgroundColor = $this.dataset.mnspSlideCtaHoverBgColor;
+			}, false );
+
+			cta.addEventListener( 'mouseout', function() {
+				$this.querySelector( '.mnsp-slide-cta' ).style.backgroundColor = $this.dataset.mnspSlideCtaBgColor;
+			}, false );
+		}
+
+		$this.style.backgroundColor = $this.dataset.mnspSlideBgColor;
+
+		if ( featured ) {
+			featured.style.opacity = $this.dataset.mnspSlideFeaturedOpacity;
+		}
+
+	} ); // eslint-disable-line
+
 } () );
