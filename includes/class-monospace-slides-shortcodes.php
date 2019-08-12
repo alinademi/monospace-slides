c659619fcfe2fa230c22391d6894f9bbb81b9ebd
Mike <mikeinmonospace@gmail.com>
2019-08-12 12:59:40 +0300

    Create the class file for the shortcodes functionality.

---
+++ includes/class-monospace-slides-shortcodes.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The functionality for creating and managing shortcodes.
+ *
+ * @link       https://www.mikeinmonospace.com/
+ * @since      1.0.0
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/includes
+ */
+
+/**
+ * The functionality for creating and managing shortcodes.
+ *
+ * Defines all the functionality related to the creation and managing of shortcodes.
+ *
+ * @package    Monospace_Slides
+ * @subpackage Monospace_Slides/includes
+ * @author     Mike In Monospace <mikeinmonospace@gmail.com>
+ */
+class Monospace_Slides_Shortcodes {
+
+
+
+}
